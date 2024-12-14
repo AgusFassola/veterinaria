d7382edfddcda070bdc00d28f64c90540f6d106b
AgusFassola <agusfassolacarp@gmail.com>
2024-12-14 12:23:17 -0300

    actualizacion

---
+++ index.js
@@ -0,0 +1,9 @@
+//variables
+const mascota = document.querySelector('#mascota');
+const propietario = document.querySelector('#propietario');
+const telefono = document.querySelector('#telefono');
+const fecha = document.querySelector('#fecha');
+const hora = document.querySelector('#hora');
+const sintomas = document.querySelector('#sintomas');
+
+//listeners
